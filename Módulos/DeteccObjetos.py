fb0a3daa723e0d0f0231db4ec0fe880df835fd73
davidQS96 <36705742+davidQS96@users.noreply.github.com>
2020-10-22 15:23:20 -0600

    Update DeteccObjetos.py

--- Módulos/DeteccObjetos.py
+++ Módulos/DeteccObjetos.py
@@ -133,6 +133,19 @@ def contornos(nombre):
         contours, _ = cv2.findContours(masko, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         #Detección de objeto y ubicación de pistas
+
+
+def contornos(cap):
+    while True:
+        ret, frame = cap.read()
+        blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([38, 86, 0])
+        upper_blue = np.array([121,255,255])
+        mask = cv2.inRange(hsv,lower_blue,upper_blue)
+    
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	
         for cnt in contours:
             area = cv2.contourArea(cnt)
             approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
